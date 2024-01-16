25a5d7354e0870fa63c1ae624f4e9ec4cce3975e
rudinawang <rudnawang07@gmail.com>
2024-01-16 14:15:51 +0545

    add fetchRecipe

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,30 +1,43 @@
+import axios from "axios";
 import Navbar from "../../Component/Navbar";
 import "./Home.css";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const fecthRecipe = async () => {
+    const response = await axios.get(
+      "https://65a56cc452f07a8b4a3f15a9.mockapi.io/recipes"
+    );
+    console.log(response);
+    setRecipes(response.data);
+  };
+
+  useEffect(() => {
+    fecthRecipe();
+  }, []);
   return (
     <div>
       <Navbar />
-      <div className="recipe-card">
-        <img
-          className="recipe-image"
-          src="your_recipe_image_url.jpg"
-          alt="Recipe Image"
-        />
-        <div className="recipe-details">
-          <h2 className="recipe-name">Delicious Recipe</h2>
-          <p className="recipe-description">
-            Biryani is a celebration of all that is great about Indian food –
-            the heady aromas, the vibrant colours, the fluffy rice and those
-            addictive curry flavours. Make this Chicken Biryani with your
-            protein of choice – or try a vegetable biryani!
-          </p>
-          <h3 className="recipe-type">Country Name</h3>
-          <a href="#" className="recipe-details-button">
-            Recipe Details
-          </a>
-        </div>
-      </div>
+      {recipes.map((recipe) => {
+        return (
+          <div key={recipe.id} className="recipe-card">
+            <img
+              className="recipe-image"
+              src={recipe.recipeImage}
+              alt="Recipe Image"
+            />
+            <div className="recipe-details">
+              <h2 className="recipe-name">{recipe.recipeName}</h2>
+              <p className="recipe-description">{recipe.recipeDescription}</p>
+              <h3 className="recipe-type">{recipe.recipeType}</h3>
+              <a href="#" className="recipe-details-button">
+                Recipe Details
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
