ce10e99d9988a598522723c9a1b2218c3089cd1a
rudinawang <rudnawang07@gmail.com>
2024-01-21 23:57:58 +0545

    adding Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import AddRecipe from "./Pages/AddRecipe/AddRecipe";
+import EditProduct from "./Pages/EditProduct/EditProduct";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addRecipe" element={<AddRecipe />} />
+          <Route path="/singleProduct/:id" element={<SingleProduct />} />
+          <Route path="/editProduct/:id" element={<EditProduct />} />
         </Routes>
       </BrowserRouter>
     </>
